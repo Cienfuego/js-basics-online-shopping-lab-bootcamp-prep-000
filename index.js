ec3fcad54c507054fac69528ddc36571458ee0ee
Michael Tompkins <mtompkins82@gmail.com>
2018-08-13 22:32:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item){
 
 function placeOrder(cardNumber) {
   if(arguments.length === 0){
-    return 'Sorry, we don\'t have a credit card on file  for you.'
+    return 'Sorry, we don\'t have a credit card on file for you.'
   }else{
     var cartTotal = total()
     cart.length = 0;
